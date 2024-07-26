4ebbe1069546e9e652d3022dc7aeb1093b747370
francososa <fm.sosa01@gmail.com>
2024-07-26 17:10:16 -0300

    se formatearon los archivos para una mayor legibilidad

--- pre-entrega-1/src/app.js
+++ pre-entrega-1/src/app.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const ProductManager = require('./controllers/ProductManager')
-const CartManager = require('./controllers/CartManager')
+const express = require('express');
+const ProductManager = require('./controllers/ProductManager');
+const CartManager = require('./controllers/CartManager');
 
-const app = express()
-const PORT = 8080
+const app = express();
+const PORT = 8080;
 
-const productManager = new ProductManager('./src/models/products.json')
-const cartManager = new CartManager('./src/models/carts.json')
+const productManager = new ProductManager('./src/models/products.json');
+const cartManager = new CartManager('./src/models/carts.json');
 
 //Middleware
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Rutas de productos
-app.use('/api/products', require('./routes/products.router')(productManager))
+app.use('/api/products', require('./routes/products.router')(productManager));
 
 // Rutas de carritos
-app.use('/api/carts', require('./routes/cart.router')(cartManager, productManager))
+app.use(
+    '/api/carts',
+    require('./routes/cart.router')(cartManager, productManager)
+);
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
